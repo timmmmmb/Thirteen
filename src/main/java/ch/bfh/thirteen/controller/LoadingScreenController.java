72e1b3baf79e8344fa1d42e54e35acd4bb8ec2c3
Tim Frey <timalexander.frey@students.bfh.ch>
2019-04-04 17:45:00 +0200

    added a animation to the loading screen image

--- src/main/java/ch/bfh/thirteen/controller/LoadingScreenController.java
+++ src/main/java/ch/bfh/thirteen/controller/LoadingScreenController.java
@@ -2,9 +2,12 @@ package main.java.ch.bfh.thirteen.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import javafx.animation.RotateTransition;
 import javafx.fxml.FXML;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.util.Duration;
 
 public class LoadingScreenController {
 
@@ -25,5 +28,10 @@ public class LoadingScreenController {
         assert hbox != null : "fx:id=\"hbox\" was not injected: check your FXML file 'loadingScreen.fxml'.";
         assert loadImage != null : "fx:id=\"loadImage\" was not injected: check your FXML file 'loadingScreen.fxml'.";
 
+        RotateTransition rt = new RotateTransition(Duration.millis(3000), loadImage);
+        rt.setByAngle(360);
+        rt.setCycleCount(10);
+
+        rt.play();
     }
 }
