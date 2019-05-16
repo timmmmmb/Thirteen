5addc422b59e82716138e23ccaaa6d15729fe172
Tim Frey <timalexander.frey@students.bfh.ch>
2019-05-16 09:50:05 +0200

    added comments to stack

--- src/main/java/ch/bfh/thirteen/stack/SizedStack.java
+++ src/main/java/ch/bfh/thirteen/stack/SizedStack.java
@@ -7,15 +7,30 @@ import java.util.Stack;
 public class SizedStack<T> extends Stack<T> {
     private int maxSize;
 
+    /**
+     * create a new stack with a maximum size
+     *
+     * @param size the size of the stack
+     */
     public SizedStack(int size) {
         super();
         this.maxSize = size;
     }
 
+    /**
+     * this constuctor is for the jaxb
+     */
+    @SuppressWarnings("unused")
     public SizedStack() {
         this(10);
     }
 
+    /**
+     * pushes an object ontop of the stack if the stack has reached its maxsize it removes its oldest element
+     *
+     * @param object the object to get added to the stack
+     * @return the argument
+     */
     @Override
     public T push(T object) {
         //If the stack is too big, remove elements until it's the right size.
