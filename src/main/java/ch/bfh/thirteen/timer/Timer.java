20ee21fa6a2cb2fa95fa989f4843480820075cd4
Tim Frey <timalexander.frey@students.bfh.ch>
2019-05-16 09:53:00 +0200

    added javadoc to the timer

--- src/main/java/ch/bfh/thirteen/timer/Timer.java
+++ src/main/java/ch/bfh/thirteen/timer/Timer.java
@@ -12,36 +12,60 @@ import java.beans.PropertyChangeSupport;
 
 @XmlRootElement(name = "timer")
 public class Timer {
-    private CustomPropertyChangeSupport pcs = new CustomPropertyChangeSupport(this);
     @XmlElement(name = "t")
     private int t = 0;
     private Timeline tl;
+    private CustomPropertyChangeSupport pcs = new CustomPropertyChangeSupport(this);
 
+    /**
+     * the timer counts the variable t up by 1 each second
+     */
     public Timer() {
         tl = new Timeline(new KeyFrame(Duration.millis(1000), e -> pcs.firePropertyChange("t", t, ++t)));
         tl.setCycleCount(Animation.INDEFINITE);
     }
 
+    /**
+     * restarts the timer
+     */
     public void restart() {
         t = 0;
     }
 
+    /**
+     * gets the pcs
+     * @return the pcs from the timer
+     */
     public PropertyChangeSupport getPcs() {
         return pcs;
     }
 
+    /**
+     * the toString function returns the current t as string
+     * @return String t
+     */
     public String toString() {
         return String.valueOf(t);
     }
 
+    /**
+     * gets the time as int
+     * @return the time as int
+     */
     public int getTime() {
         return t;
     }
 
+    /**
+     * pauses the timeline
+     */
     public void pause() {
         tl.pause();
     }
 
+    /**
+     * starts the timeline
+     */
     public void play() {
         tl.play();
     }
