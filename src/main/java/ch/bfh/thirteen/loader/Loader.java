67c1697c46f662522362edcf711e38a4079d6453
Tim Frey <timalexander.frey@students.bfh.ch>
2019-05-17 08:52:15 +0200

    added javadoc

--- src/main/java/ch/bfh/thirteen/loader/Loader.java
+++ src/main/java/ch/bfh/thirteen/loader/Loader.java
@@ -12,17 +12,32 @@ import java.io.FileReader;
 public class Loader {
     /**
      * loads from file and returns a new game
+     *
      * @return a game loaded from the savedata
      */
     public static Game loadGame() throws JAXBException, FileNotFoundException {
         return (Game) load("ThirteenSave.xml", JAXBContext.newInstance(Game.class));
     }
 
+    /**
+     * loads an object from an xml file
+     *
+     * @param filename the name of the file
+     * @param context  a jaxbcontext
+     * @return an Object that was generated from the xml
+     * @throws JAXBException         thrown if jaxb throws an exception
+     * @throws FileNotFoundException thrown if the file was not found
+     */
     public static Object load(String filename, JAXBContext context) throws JAXBException, FileNotFoundException {
         Unmarshaller um = context.createUnmarshaller();
         return um.unmarshal(new FileReader(filename));
     }
 
+    /**
+     * loads from file and returns a new settings object
+     *
+     * @return a Settings object loaded from the savedata
+     */
     public static Settings loadSettings() throws JAXBException, FileNotFoundException {
         return (Settings) load("Settings.xml", JAXBContext.newInstance(Settings.class));
     }
