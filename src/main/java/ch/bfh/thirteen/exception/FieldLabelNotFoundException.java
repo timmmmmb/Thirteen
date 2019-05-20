be05974498350f782a317e7a543d1a2f7b2957fb
Tim Frey <timalexander.frey@students.bfh.ch>
2019-05-20 10:58:34 +0200

    added javadoc to the exceptions

--- src/main/java/ch/bfh/thirteen/exception/FieldLabelNotFoundException.java
+++ src/main/java/ch/bfh/thirteen/exception/FieldLabelNotFoundException.java
@@ -4,6 +4,10 @@ package main.java.ch.bfh.thirteen.exception;
  * use this exception if a field was not found in the ui
  */
 public class FieldLabelNotFoundException extends Exception {
+    /**
+     * creates the exception
+     * @param message String the message to get displayed in the exception
+     */
     public FieldLabelNotFoundException(String message){
         super(message);
     }
--- src/main/java/ch/bfh/thirteen/exception/UINotMatchingModelException.java
+++ src/main/java/ch/bfh/thirteen/exception/UINotMatchingModelException.java
@@ -1,6 +1,13 @@
 package main.java.ch.bfh.thirteen.exception;
 
+/**
+ * use this exception if the ui doesnt match the model
+ */
 public class UINotMatchingModelException extends Exception{
+    /**
+     * creates the exception
+     * @param message String the message to get displayed in the exception
+     */
     public UINotMatchingModelException(String message){
         super(message);
     }
