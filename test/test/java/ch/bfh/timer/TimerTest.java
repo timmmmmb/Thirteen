198be59a4ced5040345344c79e8559d0f8123284
Tim Frey <timalexander.frey@students.bfh.ch>
2019-05-23 10:21:02 +0200

    added basics to test the timer

---
+++ test/test/java/ch/bfh/timer/TimerTest.java
@@ -0,0 +1,31 @@
+package test.java.ch.bfh.timer;
+
+import main.java.ch.bfh.thirteen.timer.Timer;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TimerTest {
+
+    /**
+     * tests if the timer counts correctly
+     */
+    @Test
+    void numbers() {
+        Timer t =new Timer();
+        /*t.play();
+        Thread.sleep(1000);
+        assertEquals(1, t.getTime());*/
+        t.restart();
+        assertEquals(0, t.getTime());
+    }
+
+    /**
+     * tests the constructor
+     */
+    @Test
+    void constructor(){
+        Timer t =new Timer();
+        assertEquals(0, t.getTime());
+    }
+}
