9c5d5d34d82e362993ccb026c1ca0b36f54de67c
Tim Frey <timalexander.frey@students.bfh.ch>
2019-05-28 11:44:04 +0200

    added more testfx tests for the ui

--- test/test/java/ch/bfh/controller/ControllerTest.java
+++ test/test/java/ch/bfh/controller/ControllerTest.java
@@ -1,8 +1,11 @@
 package test.java.ch.bfh.controller;
 
 import javafx.scene.control.ToggleButton;
+import javafx.scene.input.KeyCode;
 import javafx.stage.Stage;
 import main.java.ch.bfh.thirteen.application.ThirteenApplication;
+import main.java.ch.bfh.thirteen.model.Game;
+import main.java.ch.bfh.thirteen.model.GameState;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.testfx.api.FxRobot;
@@ -13,6 +16,7 @@ import org.testfx.framework.junit5.Start;
 import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
 
 @ExtendWith(ApplicationExtension.class)
 public class ControllerTest {
@@ -49,5 +53,76 @@ public class ControllerTest {
         Assertions.assertThat(((ToggleButton)robot.lookup("#musicButton").queryParent())).hasText("off");
         robot.clickOn("#musicButton");
         Assertions.assertThat(((ToggleButton)robot.lookup("#musicButton").queryParent())).hasText("on");
+        robot.clickOn("#infoButton");
+        //it is here in the info screen
+        Assertions.assertThat(robot.lookup("#backButton").queryButton()).hasText("Back");
+        robot.clickOn("#backButton");
+
+        // goes back to the menu
+        Assertions.assertThat(robot.lookup("#backButton").queryButton()).hasText("Back");
+        robot.clickOn("#backButton");
     }
+
+    @Test
+    void statisticsTest(FxRobot robot) {
+        robot.clickOn("#hiScoreButton");
+
+        Assertions.assertThat(robot.lookup("#backButton").queryButton()).hasText("Back");
+        robot.clickOn("#backButton");
+    }
+
+    @Test
+    void simulationTest(FxRobot robot) {
+        robot.clickOn("#autoplayButton");
+        Assertions.assertThat(robot.lookup("#startButton").queryButton()).hasText("Start");
+        Assertions.assertThat(robot.lookup("#restartButton").queryButton()).hasText("Restart");
+        Game currentgame = ThirteenApplication.getGame();
+        robot.clickOn("#restartButton");
+        assertSame(ThirteenApplication.getGame(), currentgame);
+        robot.clickOn("#startButton");
+        //wait until the game is over
+        while(ThirteenApplication.getGame().getBoard().getGameState()!= GameState.LOST&&ThirteenApplication.getGame().getBoard().getGameState()!= GameState.WON){
+            robot.sleep(500);
+        }
+        robot.clickOn("#botSelector");
+        robot.type(KeyCode.DOWN);
+        robot.type(KeyCode.ENTER);
+
+        robot.sleep(1000);
+        robot.clickOn("#startButton");
+        //wait until the game is over
+        while(ThirteenApplication.getGame().getBoard().getGameState()!= GameState.LOST&&ThirteenApplication.getGame().getBoard().getGameState()!= GameState.WON){
+            robot.sleep(500);
+        }
+
+        robot.clickOn("#botSelector");
+        robot.type(KeyCode.DOWN);
+        robot.type(KeyCode.ENTER);
+        robot.clickOn("#startButton");
+        //wait until the game is over
+        while(ThirteenApplication.getGame().getBoard().getGameState()!= GameState.LOST&&ThirteenApplication.getGame().getBoard().getGameState()!= GameState.WON){
+            robot.sleep(500);
+        }
+        Assertions.assertThat(robot.lookup("#menuButton").queryButton()).hasText("");
+        robot.clickOn("#menuButton");
+    }
+
+    @Test
+    void gameTest(FxRobot robot) {
+        robot.clickOn("#startButton");
+
+        Assertions.assertThat(robot.lookup("#menuButton").queryButton()).hasText("");
+        robot.clickOn("#menuButton");
+    }
+
+    @Test
+    void loadTest(FxRobot robot) {
+        robot.clickOn("#saveButton");
+    }
+
+    @Test
+    void saveTest(FxRobot robot) {
+        robot.clickOn("#loadButton");
+    }
+
 }
